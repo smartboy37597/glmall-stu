ea318832d7c29e3624fd5a71c5f842866588c036
smartboy <1181882120@qq.com>
2023-04-10 22:10:12 +0800

    项目初始结构

---
+++ glmall-order/src/main/java/com/smtboy/glmall/order/GlmallOrderApplication.java
@@ -0,0 +1,13 @@
+package com.smtboy.glmall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlmallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlmallOrderApplication.class, args);
+    }
+
+}
